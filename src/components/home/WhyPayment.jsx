ddb8d8380c76b2c46cf58d8034d29e55ed3f92ad
SushanGhimire <sushangmi55@gmail.com>
2021-03-26 10:18:20 +0545

    why Payment updated

--- src/components/home/WhyPayment.jsx
+++ src/components/home/WhyPayment.jsx
@@ -9,8 +9,8 @@ function WhyPayment() {
       }}
     >
       <div className="w-full h-full bg-black absolute bg-opacity-50"></div>
-      <div className="absolute w-full flex justify-center flex-col items-center space-y-4 top-56 right-0">
-        <div className="font-bold text-white text-4xl md:text-6xl mt-10 md:mt-0 font-header">
+      <div className="absolute w-full flex justify-center flex-col items-center space-y-4 top-52 right-0">
+        <div className="font-bold text-white text-4xl md:text-6xl  font-header">
           Why Payment?
         </div>
         <div className="text-base md:text-lg font-bold text-white text-center">
