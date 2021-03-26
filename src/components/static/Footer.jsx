0e6596479c047367adc133eff4aaf79f8c122ad1
SushanGhimire <sushangmi55@gmail.com>
2021-03-26 09:16:30 +0545

    footer updated

--- src/components/static/Footer.jsx
+++ src/components/static/Footer.jsx
@@ -5,16 +5,21 @@ import twitter from "../../assets/images/homepage/twitter.svg";
 function Footer() {
   const logos = [facebook, instagram, linkedin, twitter];
   return (
-    <div className="w-full bg-secondary p-5 md:p-16 text-white flex-col">
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-6  border-b pb-5">
+    <div className="w-full bg-black p-5 md:pt-10 mt-20 text-white flex-col">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 border-b p-5">
         <div className="col-span-1 flex flex-col">
-          <div className="text-lg font-bold mx-auto">CarbonPay</div>
+          <div className="text-3xl font-bold mx-auto text-primary">
+            CarbonPay
+          </div>
+          <div className="text-center  sm:px-5 py-2 text-sm sm:text-base">
+            Removing Carbon From Environment with Every Online Purchase
+          </div>
           <div className="flex space-x-3 mx-auto py-3">
             {logos.map((logo) => {
               return (
                 <div
                   key={logo}
-                  className="rounded-full border-2 p-2 hover:bg-green-secondary transition-all duration-200"
+                  className="rounded-full border-2 p-2 hover:bg-green-secondary transition-all duration-200 hover:bg-primary cursor-pointer transform hover:-translate-y-1 hover:scale-125"
                 >
                   <img src={logo} className="w-4 h-4" alt="" />
                 </div>
@@ -23,8 +28,10 @@ function Footer() {
           </div>
         </div>
         <div className="col-span-1 mx-auto ">
-          <div className="text-lg font-bold">CarbonPay</div>
-          <ul className="mt-3 space-y-2">
+          <div className="text-2xl font-bold mx-auto text-primary">
+            CarbonPay
+          </div>
+          <ul className="mt-3 space-y-2 text-center text-sm sm:text-base">
             <li>CarbonPay 1</li>
             <li>CarbonPay 2</li>
             <li>CarbonPay 3</li>
@@ -33,8 +40,10 @@ function Footer() {
           </ul>
         </div>
         <div className="col-span-1 mx-auto">
-          <div className="text-lg font-bold">Developers</div>
-          <ul className="mt-3 space-y-2">
+          <div className="text-2xl font-bold mx-auto text-primary">
+            Developers
+          </div>
+          <ul className="mt-3 space-y-2 text-center  text-sm sm:text-base">
             <li>Developers 1</li>
             <li>Developers 2</li>
             <li>Developers 3</li>
